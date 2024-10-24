0dc5e7e60668ee1554dd0636cbcd9a8eacde5e16
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-24 08:51:34 +0200

    Feat: finish component PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,11 @@
 export default function PokemonCard() {
 	return (
 		<figure>
-			<figcaption>
-				<img
-					src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-					alt=""
-				/>
-				Bulbasaur
-			</figcaption>
+			<img
+				src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+				alt=""
+			/>
+			<figcaption>Bulbasaur</figcaption>
 		</figure>
 	);
 }
