210b66fa64e0ba5ac2616013b29d8b802c2f8dd9
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-24 08:44:52 +0200

    Sauvegarde 24.10 matin

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+export default function PokemonCard() {
+	return (
+		<figure>
+			<figcaption>
+				<img
+					src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+					alt=""
+				/>
+				Bulbasaur
+			</figcaption>
+		</figure>
+	);
+}
