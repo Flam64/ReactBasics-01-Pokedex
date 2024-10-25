c367b875c4127ca788e9951fe5266c8a3a2eb0de
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-25 07:02:50 +0200

    Feat: finish challenge React - Basics - 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
 	{
@@ -10,15 +11,60 @@ const pokemonList = [
 	},
 
 	{
+		name: "charmander",
+
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+
+	{
+		name: "squirtle",
+
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+
+	{
+		name: "pikachu",
+
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+
+	{
 		name: "mew",
 	},
 ];
 
 function App() {
+	const [pokemonIndex, setpokemonIndex] = useState(0);
+
+	const handleClickSuivant = () => {
+		setpokemonIndex(pokemonIndex + 1);
+	};
+
+	const handleClickPecedent = () => {
+		setpokemonIndex(pokemonIndex - 1);
+	};
+
 	return (
-		<div>
-			<PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
-		</div>
+		<>
+			<PokemonCard
+				imgSrc={pokemonList[pokemonIndex].imgSrc}
+				name={pokemonList[pokemonIndex].name}
+			/>
+			{pokemonIndex > 0 && (
+				<button type="button" onClick={handleClickPecedent}>
+					Precedent
+				</button>
+			)}
+
+			{pokemonIndex < pokemonList.length - 1 && (
+				<button type="button" onClick={handleClickSuivant}>
+					Suivant
+				</button>
+			)}
+		</>
 	);
 }
 
